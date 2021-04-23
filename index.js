33345ca452e87784a93a67fe9c53f3740a8f0b8e
AdamMatolak <adam.matolak@kosickaakademia.sk>
2021-04-23 11:14:09 +0200

    completed sending new task to database

--- index.js
+++ index.js
@@ -50,27 +50,33 @@ app.get('/task', (req, res)=>{
                 console.log(result);
                 res.send(result);
         })
-        }
-        
+        } 
     })
 })
 
 app.post('/task/new', (req, res)=>{
-    const data = (req.body);
-    const name = data.name;
-    const priority = data.priority;
-    const date = currentDate;
-    let currentDate = new Date();
-    let price='undefined';
-    if(data.price){
-        price = data.price;
-    }
-    console.log(name,' ',priority,' ',price,' ',date);
-
-    
+    MongoClient.connect(connectionURL, (error, client)=>{
+        const data = (req.body);
+        const name = data.name;
+        const priority = data.priority;
+        let currentDate = new Date();
+        let price='undefined';
+        if(data.price){
+            price = data.price;
+        }
+        //console.log(name,' ',priority,' ',price,' ',currentDate);
+        var newTask = {name: name, priority: priority, price: price, done: 'false', date: currentDate};
+        const db=client.db(databaseName);
+        db.collection("Tasks").insertOne(newTask, function(err, res) {
+            if(err){
+                return console.log('Unable to add new task');
+            } else{
+                console.log("1 document inserted");
+            } 
+        })
+    })
 })
-
-
+    
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 }) 
